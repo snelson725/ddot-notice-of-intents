c5d58c6e456e23927ace3c04bf107ae86fa14d0e
SamuelNelson <Samuel.Nelson@dc.gov>
2026-03-24 11:13:04 -0400

    Callback fix.

--- js/callback.js
+++ js/callback.js
@@ -1,10 +1,14 @@
 function handleCallback() {
   const hash = new URLSearchParams(window.location.hash.substring(1));
   const token = hash.get("access_token");
-
+  
   if (token) {
     localStorage.setItem("arcgis_token", token);
-    window.location = "/ddot-notice-of-intents/";
+
+    // Redirect back to your GitHub Pages app
+    window.location = "https://snelson725.github.io/ddot-notice-of-intents/";
+  } else {
+    document.body.textContent = "Authentication failed.";
   }
 }
 
